b938f3454be3846dcad524967f7bab2e828668b3
Prateek <prateek.rokadiya@gyrix.in>
2017-11-15 18:17:35 +0530

    Created input

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
-
 import React from "react";
 import ReactDOM from "react-dom";
 
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyC3w8z3FwZKTbwekLM0xTl2K4hGo_1D644';
+
 const App = () => {
-    return <div>Hi</div>;
+    return (
+        <div>
+            <SearchBar/>
+        </div>
+    );
 }
 
 ReactDOM.render(<App/>, document.querySelector('.container'));
